baae8af2eb68aedb5d031d63e4f29dd2428fe11f
CxxxxDxxxF <obvcjgaming@gmail.com>
2025-05-14 17:15:53 -0400

    Delete gotlockz_bot.py

--- gotlockz_bot.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext import commands
-from mlb_statsapi import MLBStatsAPI
-
-bot = commands.Bot(command_prefix='!')
-
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user}')
