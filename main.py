9c3fd7ca4d842a3a19eb0e8fe5c34c3ce4a9a0b3
CxxxxDxxxF <obvcjgaming@gmail.com>
2025-05-20 16:26:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+
+if not os.path.exists("credentials.json") and os.getenv("GOOGLE_SHEETS_CREDS"):
+    with open("credentials.json", "w") as f:
+        f.write(os.environ["GOOGLE_SHEETS_CREDS"])
+
 from gotlockz_bot import bot, TOKEN
 
 if __name__ == "__main__":
