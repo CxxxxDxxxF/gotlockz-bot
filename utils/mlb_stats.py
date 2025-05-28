a81fbef17badf546bcea084789825416fb6b4281
CxxxxDxxxF <obvcjgaming@gmail.com>
2025-05-28 19:07:26 -0400

    Update mlb_stats.py

--- utils/mlb_stats.py
+++ utils/mlb_stats.py
@@ -1,4 +1,16 @@
+import statsapi  # comes from the GitHub library
+
 def get_game_stats(bet_info):
-    """Fetch game or player stats based on extracted bet_info."""
-    # TODO: Integrate mlbstatsapi calls
-    return {}
+    """Fetch game or pitcher stats via the vendored statsapi."""
+    # for example:
+    game_id = statsapi.schedule(
+        team=bet_info['team'], 
+        date=bet_info['date_str']
+    )[0]['game_id']
+    boxscore = statsapi.boxscore_data(game_id)
+    # …extract what you need, return as dict…
+    return {
+      'home_record': boxscore['home']['team_record'],
+      'away_record': boxscore['away']['team_record'],
+      # etc.
+    }
